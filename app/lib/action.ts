066b92366aef301bd4da92c3703c0705f2524e51
open1i <open160405103@163.com>
2025-03-10 17:45:34 +0800

    11.9 Server: Pass the id to the Server Action

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -35,8 +35,31 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
+
+
 /**
  * Next.js has a client-side router cache that stores the route segments in the user's browser for a time. Along with prefetching, this cache ensures that users can quickly navigate between routes while reducing the number of requests made to the server.
  * 
  * Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server. You can do this with the revalidatePath function from Next.js:
  */
+
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
