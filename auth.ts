403c130c88618d514f869b39400476d408c22b0a
open1i <open160405103@163.com>
2025-03-10 18:57:40 +0800

    14.5 validating the credentials,

--- auth.ts
+++ auth.ts
@@ -1,13 +1,22 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
 
 // When seeding your database, you used a package called bcrypt to hash the user's password before storing it in the database. You will use it again later in this chapter to compare that the password entered by the user matches the one in the database. However, you will need to create a separate file for the bcrypt package. This is because bcrypt relies on Node.js APIs not available in Next.js Middleware.
 
 // Create a new file called auth.ts that spreads your authConfig object:
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [Credentials({})],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+      },
+    }),
+  ],
 });
 
 // There are other alternative providers such as OAuth
